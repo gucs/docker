a36010e5fb87b8215ecfbe8a871939a9d2e39b5d
Jonathan <gucs.net@gmail.com>
2019-07-12 00:06:05 +0800

    no message

--- mysql80/docker-entrypoint-initdb.d/remote-access.sql
+++ mysql80/docker-entrypoint-initdb.d/remote-access.sql
@@ -1,2 +1,7 @@
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '123456' WITH GRANT OPTION;
+-- https://stackoverflow.com/questions/50177216/how-to-grant-all-privileges-to-root-user-in-mysql-8-0
+-- Starting with MySQL 8 you no longer can (implicitly) create a user using the GRANT command.
+-- Use CREATE USER instead, followed by the GRANT statement:
+
+CREATE USER 'root'@'%' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
